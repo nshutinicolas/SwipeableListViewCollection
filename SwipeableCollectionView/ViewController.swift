cb7bbf4568766e9dd5b300f43785c12c89a4806d
nshutinicolas <nshuti.nicolas@yahoo.com>
2022-07-01 14:37:57 +0200

    Initial Commit

---
+++ SwipeableCollectionView/ViewController.swift
@@ -0,0 +1,21 @@
+//
+// ViewController.swift
+// SwipeableCollectionView
+//
+// Created by Nicolas Nshuti on 01/07/2022
+// Copyright © 2022 GHOST TECHNOLOGIES LLC. All rights reserved.
+//
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
